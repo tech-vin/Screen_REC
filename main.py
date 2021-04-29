bd88b9cdfb716e5822a0fd3e2f4a00b4430635e3
tech-vin <tech.vineetsingh@gmail.com>
2021-04-30 00:40:51 +0530

    file uploaded

---
+++ main.py
@@ -0,0 +1,23 @@
+import cv2
+import numpy as np
+import pyautogui
+import time
+
+SCREEN_SIZE = (1920, 1080)
+fource = cv2.VideoWriter_fourcc(*'XVID')
+out = cv2.VideoWriter('output.avi', fource, 20.0, (SCREEN_SIZE))
+
+fps =30
+prev = 0
+
+while True:
+    time_elapsed = time.time()-prev
+    img = pyautogui.screenshot()
+    if time_elapsed > 1.0/fps:
+        prev = time.time()
+        frame = np.array(img)
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        out.write(frame)
+    cv2.waitKey(1000)
+cv2.destroyAllWindows()
+out.release()
